bda9cc1918a51b816b4abbd8e0fb346c163aeae7
LucidZ <ljz@u.northwestern.edu>
2025-05-08 19:46:54 -0600

    some fixes to router

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/" : "/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@projects": path.resolve(__dirname, "./src/projects"),
+    },
+  },
 });
