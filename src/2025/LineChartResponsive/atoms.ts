8f6a76a92eb55deb2bbd702715593c16c42d4621
LucidZ <ljz@u.northwestern.edu>
2025-10-30 14:18:37 -0600

    delete line chart responsive
    this was a scratch pad

--- src/2025/LineChartResponsive/atoms.ts
+++
@@ -1,7 +0,0 @@
-import { atom } from "jotai";
-
-export const dataAtom = atom(null);
-
-export const guessAtom = atom(0);
-export const hasInteractedAtom = atom(false);
-export const hasSubmittedAtom = atom(false);
