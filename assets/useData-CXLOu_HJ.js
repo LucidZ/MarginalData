b4c148228bd6a9e8735efbef60964c0e1e1bd137
LucidZ <ljz@u.northwestern.edu>
2026-08-24 21:09:56 -0600

    Updates

---
+++ assets/useData-CXLOu_HJ.js
@@ -0,0 +1,1 @@
+import{u as a}from"./useJsonData-BrAmHhRb.js";const s="/data/statsbomb/wc2022-final-passing.json",o=()=>a(s);export{o as u};
--- assets/useData-CrVcq81G.js
+++
@@ -1,1 +0,0 @@
-import{u as a}from"./useJsonData-CX6KGp9c.js";const s="/data/statsbomb/wc2022-final-passing.json",o=()=>a(s);export{o as u};
